169096d7bf6df9c20ed801e7c8f0cebee48de0e3
Manuel Raimann <raimannma@outlook.de>
2020-03-15 15:30:50 +0100

    remove tournament selection probability

--- src/main/java/methods/Selection.java
+++ src/main/java/methods/Selection.java
@@ -2,6 +2,7 @@ package methods;
 
 import static methods.Utils.pickRandom;
 import static methods.Utils.randDouble;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -52,21 +53,17 @@ public abstract class Selection {
   }
 
   public final static class Tournament extends Selection {
-    private final double probability;
     private final int size;
 
     public Tournament(final int size, final double probability) {
       this.size = size;
-      this.probability = probability;
     }
 
     @Override
     public Network select(final @NotNull List<Network> population) {
       return IntStream.range(0, Math.min(population.size(), this.size))
         .mapToObj(i -> pickRandom(population))
-        .sorted((o1, o2) -> Double.compare(o2.score, o1.score))
-        .filter(net -> randDouble() < this.probability)
-        .findFirst()
+        .max(Comparator.comparingDouble(o -> o.score))
         .orElse(pickRandom(population));
     }
   }
