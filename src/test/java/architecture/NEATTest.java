6a19392ced2911a0ce417c62e9063db4f0b99f09
Manuel Raimann <raimannma@outlook.de>
2020-03-15 11:35:37 +0100

    adding annotation for test

--- src/test/java/architecture/NEATTest.java
+++ src/test/java/architecture/NEATTest.java
@@ -5,144 +5,145 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.stream.IntStream;
 import methods.Mutation;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NEATTest {
-  @Test
-  public void testJSON() {
-    final Network original = new Network(2, 2);
-    IntStream.range(0, 10)
-      .mapToObj(i -> pickRandom(Mutation.ALL))
-      .forEach(original::mutate);
+    private static double learnSet(final @NotNull Network network, final double[][] inputs, final double[][] outputs) {
+        final EvolveOptions options = new EvolveOptions();
+        options.setMutations(Mutation.ALL);
+        options.setPopulationSize(1000);
+        options.setElitism(10);
+        options.setMutationRate(0.7);
+        options.setError(0.05);
+        return network.evolve(inputs, outputs, options);
+    }
 
-    final Network copied = Network.fromJSON(original.toJSON());
+    @Test
+    public void testJSON() {
+        final Network original = new Network(2, 2);
+        IntStream.range(0, 10)
+                .mapToObj(i -> pickRandom(Mutation.ALL))
+                .forEach(original::mutate);
 
-    assertEquals(original.input, copied.input);
-    assertEquals(original.output, copied.output);
-    original.nodes
-      .parallelStream()
-      .map(node -> copied.nodes.contains(node))
-      .forEach(Assertions::assertTrue);
-  }
+        final Network copied = Network.fromJSON(original.toJSON());
 
-  @Test
-  public void testAND() {
-    final double[][] inputs = new double[][] {
-      new double[] {0, 0},
-      new double[] {0, 1},
-      new double[] {1, 0},
-      new double[] {1, 1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {0},
-      new double[] {0},
-      new double[] {0},
-      new double[] {1},
-    };
+        assertEquals(original.input, copied.input);
+        assertEquals(original.output, copied.output);
+        original.nodes
+                .parallelStream()
+                .map(node -> copied.nodes.contains(node))
+                .forEach(Assertions::assertTrue);
+    }
 
-    final Network network = new Network(2, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+    @Test
+    public void testAND() {
+        final double[][] inputs = new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 1},
+                new double[]{1, 0},
+                new double[]{1, 1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{0},
+                new double[]{0},
+                new double[]{0},
+                new double[]{1},
+        };
 
-  private static double learnSet(final Network network, final double[][] inputs, final double[][] outputs) {
-    final EvolveOptions options = new EvolveOptions();
-    options.setMutations(Mutation.ALL);
-    options.setPopulationSize(1000);
-    options.setElitism(10);
-    options.setMutationRate(0.7);
-    options.setError(0.05);
-    return network.evolve(inputs, outputs, options);
-  }
+        final Network network = new Network(2, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 
-  @Test
-  public void testXOR() {
-    final double[][] inputs = new double[][] {
-      new double[] {0, 0},
-      new double[] {0, 1},
-      new double[] {1, 0},
-      new double[] {1, 1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {0},
-      new double[] {1},
-      new double[] {1},
-      new double[] {0},
-    };
+    @Test
+    public void testXOR() {
+        final double[][] inputs = new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 1},
+                new double[]{1, 0},
+                new double[]{1, 1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{0},
+                new double[]{1},
+                new double[]{1},
+                new double[]{0},
+        };
 
-    final Network network = new Network(2, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+        final Network network = new Network(2, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 
-  @Test
-  public void testXNOR() {
-    final double[][] inputs = new double[][] {
-      new double[] {0, 0},
-      new double[] {0, 1},
-      new double[] {1, 0},
-      new double[] {1, 1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {1},
-      new double[] {0},
-      new double[] {0},
-      new double[] {1},
-    };
+    @Test
+    public void testXNOR() {
+        final double[][] inputs = new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 1},
+                new double[]{1, 0},
+                new double[]{1, 1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{1},
+                new double[]{0},
+                new double[]{0},
+                new double[]{1},
+        };
 
-    final Network network = new Network(2, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+        final Network network = new Network(2, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 
-  @Test
-  public void testNot() {
-    final double[][] inputs = new double[][] {
-      new double[] {0},
-      new double[] {1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {1},
-      new double[] {0},
-    };
+    @Test
+    public void testNot() {
+        final double[][] inputs = new double[][]{
+                new double[]{0},
+                new double[]{1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{1},
+                new double[]{0},
+        };
 
-    final Network network = new Network(1, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+        final Network network = new Network(1, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 
-  @Test
-  public void testNAND() {
-    final double[][] inputs = new double[][] {
-      new double[] {0, 0},
-      new double[] {0, 1},
-      new double[] {1, 0},
-      new double[] {1, 1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {1},
-      new double[] {1},
-      new double[] {1},
-      new double[] {0},
-    };
+    @Test
+    public void testNAND() {
+        final double[][] inputs = new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 1},
+                new double[]{1, 0},
+                new double[]{1, 1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{1},
+                new double[]{1},
+                new double[]{1},
+                new double[]{0},
+        };
 
-    final Network network = new Network(2, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+        final Network network = new Network(2, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 
-  @Test
-  public void testNOR() {
-    final double[][] inputs = new double[][] {
-      new double[] {0, 0},
-      new double[] {0, 1},
-      new double[] {1, 0},
-      new double[] {1, 1},
-    };
-    final double[][] outputs = new double[][] {
-      new double[] {1},
-      new double[] {0},
-      new double[] {0},
-      new double[] {0},
-    };
+    @Test
+    public void testNOR() {
+        final double[][] inputs = new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 1},
+                new double[]{1, 0},
+                new double[]{1, 1},
+        };
+        final double[][] outputs = new double[][]{
+                new double[]{1},
+                new double[]{0},
+                new double[]{0},
+                new double[]{0},
+        };
 
-    final Network network = new Network(2, 1);
-    assertTrue(learnSet(network, inputs, outputs) <= 0.05);
-  }
+        final Network network = new Network(2, 1);
+        assertTrue(learnSet(network, inputs, outputs) <= 0.05);
+    }
 }
