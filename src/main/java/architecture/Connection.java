6b7841db4a66ee5e3cef584b5049f546aaa0434d
Manuel Raimann <raimannma@outlook.de>
2020-03-18 11:16:22 +0100

    adding comments for Connection
    build javadoc

--- src/main/java/architecture/Connection.java
+++ src/main/java/architecture/Connection.java
@@ -3,13 +3,40 @@ package architecture;
 import com.google.gson.JsonObject;
 import java.util.Objects;
 
+/**
+ * The type Connection.
+ */
 public class Connection {
+  /**
+   * The output node of this connection.
+   */
   public final Node to;
+  /**
+   * The input node of this connection.
+   */
   public final Node from;
+  /**
+   * The node which gates this connection.
+   */
   public Node gateNode;
+  /**
+   * The weight of this connection.
+   */
   public double weight;
+  /**
+   * The connection gain.
+   * <p>
+   * Used for gating, gets multiplied with weight
+   */
   public double gain;
 
+  /**
+   * Instantiates a new Connection.
+   *
+   * @param from   The input node of this connection.
+   * @param to     The output node of this connection.
+   * @param weight The weight of this connection.
+   */
   Connection(final Node from, final Node to, final double weight) {
     this.from = from;
     this.to = to;
@@ -18,10 +45,26 @@ public class Connection {
     this.gateNode = null;
   }
 
-  public static int getInnovationID(final int a, final int b) {
-    return (int) Math.floor(0.5 * (a + b) * (a + b + 1) + b);
+  /**
+   * Calculates the innovation id.
+   *
+   * @param nodeIndexFrom   the nodeIndexFrom
+   * @param nodeIndexTarget the nodeIndexTarget
+   * @return An Integer that uniquely represents a pair of Integers
+   * @see <a href="https://en.wikipedia.org/wiki/Pairing_function">(Cantor pairing function)|Pairing function (Cantor pairing function)}</a>
+   */
+  public static int getInnovationID(final int nodeIndexFrom, final int nodeIndexTarget) {
+    return (int) Math.floor(0.5
+      * (nodeIndexFrom + nodeIndexTarget)
+      * (nodeIndexFrom + nodeIndexTarget + 1)
+      + nodeIndexTarget);
   }
 
+  /**
+   * Converts a node to JsonObject
+   *
+   * @return the connection as JsonObject
+   */
   public JsonObject toJSON() {
     final JsonObject jsonObject = new JsonObject();
     jsonObject.addProperty("weight", this.weight);
