32aba9f53f6a8e934368b8a207785057ad0e9b9d
M. Raimann <raimannma@outlook.de>
2020-03-22 13:52:58 +0100

    refactor testJSON

--- src/test/java/architecture/NEATTest.java
+++ src/test/java/architecture/NEATTest.java
@@ -3,7 +3,9 @@ package architecture;
 import static methods.Utils.pickRandom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.IntStream;
 import methods.Mutation;
 import org.jetbrains.annotations.NotNull;
@@ -33,18 +35,22 @@ public class NEATTest {
 		final List<Connection> copiedConnections = new ArrayList<>(copied.connections);
 		final List<Connection> originalSelfConnections = new ArrayList<>(original.selfConnections);
 		final List<Connection> copiedSelfConnections = new ArrayList<>(copied.selfConnections);
+		final List<Connection> originalGates = new ArrayList<>(original.gates);
+		final List<Connection> copiedGates = new ArrayList<>(copied.gates);
 
 		originalConnections.sort(Comparator.comparingDouble(conn -> conn.weight));
 		copiedConnections.sort(Comparator.comparingDouble(conn -> conn.weight));
 		originalSelfConnections.sort(Comparator.comparingDouble(conn -> conn.weight));
 		copiedSelfConnections.sort(Comparator.comparingDouble(conn -> conn.weight));
+		originalGates.sort(Comparator.comparingDouble(conn -> conn.weight));
+		copiedGates.sort(Comparator.comparingDouble(conn -> conn.weight));
 
 		assertEquals(original.input, copied.input);
 		assertEquals(original.output, copied.output);
 		assertEquals(original.nodes, copied.nodes);
 		assertEquals(originalConnections, copiedConnections);
 		assertEquals(originalSelfConnections, copiedSelfConnections);
-		assertEquals(original.gates, copied.gates);
+		assertEquals(originalGates, copiedGates);
 	}
 
 	@Test
