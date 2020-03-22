c57d43611b0b4a142e7e2a02e7be1a6c7a2d630b
M. Raimann <raimannma@outlook.de>
2020-03-22 19:35:07 +0100

    no need to set special parameters in NEATTest.learnSet

--- src/test/java/architecture/NEATTest.java
+++ src/test/java/architecture/NEATTest.java
@@ -12,10 +12,6 @@ import org.junit.jupiter.api.Test;
 public class NEATTest {
 	private static double learnSet(final @NotNull Network network, final double[][] inputs, final double[][] outputs) {
 		final EvolveOptions options = new EvolveOptions();
-		options.setMutations(Mutation.ALL);
-		options.setPopulationSize(100);
-		options.setElitism(10);
-		options.setMutationRate(0.7);
 		options.setError(0.05);
 		return network.evolve(inputs, outputs, options);
 	}
