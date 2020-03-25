0a613134982e8d0e83d0ff5d3d22fee138d51cdd
M. Raimann <raimannma@outlook.de>
2020-03-25 19:41:39 +0100

    adding classes for sampling strategies

---
+++ src/main/java/com/github/raimannma/rl/methods/ContinuousSampling.java
@@ -0,0 +1,4 @@
+package com.github.raimannma.rl.methods;
+
+public class ContinuousSampling {
+}
---
+++ src/main/java/com/github/raimannma/rl/methods/DiscreteSampling.java
@@ -0,0 +1,28 @@
+package com.github.raimannma.rl.methods;
+
+import static com.github.raimannma.nn.methods.Utils.randDouble;
+import static com.github.raimannma.nn.methods.Utils.randInt;
+
+public abstract class DiscreteSampling {
+	public abstract int sample(int action, int episode, int numActions);
+
+	public static final class EpsilonGreedy extends DiscreteSampling {
+		private final double epsilon;
+		private final double epsilonDecay;
+		private final double epsilonMin;
+
+		public EpsilonGreedy(final double epsilon, final double epsilonDecay, final double epsilonMin) {
+			this.epsilon = epsilon;
+			this.epsilonDecay = epsilonDecay;
+			this.epsilonMin = epsilonMin;
+		}
+
+		@Override
+		public int sample(final int action, final int episode, final int numActions) {
+			final double currentEpsilon = Math.max(this.epsilonMin, this.epsilon * Math.pow(this.epsilonDecay, episode));
+			return randDouble() >= currentEpsilon
+				? action
+				: randInt(numActions);
+		}
+	}
+}
