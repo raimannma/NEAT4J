8ab6eef837e68b0db002db9ab35cd842f79c45fb
M. Raimann <raimannma@outlook.de>
2020-03-25 19:03:53 +0100

    adding simple Experience class

---
+++ src/main/java/com/github/raimannma/rl/Experience.java
@@ -0,0 +1,32 @@
+package com.github.raimannma.rl;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Experience implements Comparable<Experience> {
+	protected final int generation;
+	public double tdError;
+	protected double[] lastState;
+	protected int lastAction;
+	protected double lastReward;
+	protected int state;
+	protected int action;
+
+	public Experience(final int generation, final double[] lastState, final int lastAction, final double lastReward, final int state, final int action) {
+		this(generation, lastState, lastAction, lastReward, state, action, 0);
+	}
+
+	public Experience(final int generation, final double[] lastState, final int lastAction, final double lastReward, final int state, final int action, final double tdError) {
+		this.generation = generation;
+		this.tdError = tdError;
+		this.lastState = lastState;
+		this.lastAction = lastAction;
+		this.lastReward = lastReward;
+		this.state = state;
+		this.action = action;
+	}
+
+	@Override
+	public int compareTo(@NotNull final Experience o) {
+		return Integer.compare(this.generation, o.generation);
+	}
+}
