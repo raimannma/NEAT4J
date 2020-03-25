62221db5b0969637e9982461a9b75b31006a8b8b
M. Raimann <raimannma@outlook.de>
2020-03-25 19:22:07 +0100

    adding empty DQN class implementing the DiscreteAgent

---
+++ src/main/java/com/github/raimannma/rl/agents/DQN.java
@@ -0,0 +1,13 @@
+package com.github.raimannma.rl.agents;
+
+public class DQN implements DiscreteAgent {
+	@Override
+	public int act(final double[] state) {
+		return 0;
+	}
+
+	@Override
+	public double learn(final double reward) {
+		return 0;
+	}
+}
