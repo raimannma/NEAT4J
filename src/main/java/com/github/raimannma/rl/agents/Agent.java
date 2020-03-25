1ce2b5213d1f6e1463c124b10e98999778e00696
M. Raimann <raimannma@outlook.de>
2020-03-25 19:41:01 +0100

    adding a episode counter variable to Agent

--- src/main/java/com/github/raimannma/rl/agents/Agent.java
+++ src/main/java/com/github/raimannma/rl/agents/Agent.java
@@ -3,6 +3,7 @@ package com.github.raimannma.rl.agents;
 public abstract class Agent {
 	public int numStates;
 	public int numActions;
+	public int episode;
 
 	Agent(final int numStates, final int numActions) {
 		this.numStates = numStates;
