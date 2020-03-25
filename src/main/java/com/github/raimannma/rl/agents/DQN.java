c38cbe254be99bf1a627aff5efaed88d2105a670
M. Raimann <raimannma@outlook.de>
2020-03-25 19:29:52 +0100

    fixing DQN (now extending instead of implementing)

--- src/main/java/com/github/raimannma/rl/agents/DQN.java
+++ src/main/java/com/github/raimannma/rl/agents/DQN.java
@@ -1,6 +1,10 @@
 package com.github.raimannma.rl.agents;
 
-public class DQN implements DiscreteAgent {
+public class DQN extends DiscreteAgent {
+	public DQN(final int numStates, final int numActions) {
+		super(numStates, numActions);
+	}
+
 	@Override
 	public int act(final double[] state) {
 		return 0;
